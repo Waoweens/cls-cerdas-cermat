ffad694f7ff73dcbb5ee111b479552bc64ef2b3e
Waoweens <30313089+Waoweens@users.noreply.github.com>
2022-08-11 22:56:59 +0700

    webserver edition: part 2

--- src/main.cpp
+++ src/main.cpp
@@ -19,8 +19,8 @@
 AsyncWebServer server(80);
 AsyncWebSocket ws("/ws");
 
-const char* ssid = "KooshaWi-Fi1 2.4GHz";
-const char* password = "theawiligar144";
+const char *ssid = "KooshaWi-Fi1 2.4GHz";
+const char *password = "theawiligar144";
 
 const int p1_btn = 0;
 const int p2_btn = 2;
@@ -40,43 +40,224 @@ int p2_score = 0;
 int p3_score = 0;
 int p4_score = 0;
 
-const char *html_index = R"+(
+const char *html_index PROGMEM = R"+(
 <!DOCTYPE html>
 <html lang="en">
-	<head>
-		<link rel="preconnect" href="https://fonts.googleapis.com">
-		<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-		<link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet">
-		<title>Cerdas Cermat</title>
 
-		<style>
-			* {
-				font-family: 'Roboto', sans-serif;
+<head>
+	<link rel="preconnect" href="https://fonts.googleapis.com">
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+	<link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet">
+	<title>Cerdas Cermat</title>
+
+	<style>
+		* {
+			font-family: 'Roboto', sans-serif;
+		}
+
+		.flex-container {
+			display: flex;
+			justify-content: center;
+			align-items: center;
+
+		}
+
+		.fc-space {
+			margin-top: 25px;
+			gap: 10px;
+		}
+
+		.grid-container {
+			display: grid;
+			grid-template-columns: auto auto auto auto;
+		}
+
+		.grid-content {
+			display: flex;
+			align-items: center;
+			justify-content: center;
+			text-align: center;
+			border: 10px solid rgba(255, 255, 255, 0)
+		}
+
+		.current-player {
+			border: 10px solid rgba(0, 0, 255, 0.8) !important;
+		}
+
+		.center {
+			text-align: center;
+		}
+
+		.score {
+			font-size: 8em;
+			font-weight: bold;
+		}
+
+		.group-name {
+			font-size: 2em;
+			font-weight: bold;
+		}
+
+		.btn-correct {
+			background-color: #4CAF50;
+			/* Green */
+			border: none;
+			color: white;
+			padding: 15px 32px;
+			text-align: center;
+			text-decoration: none;
+			display: inline-block;
+			font-size: 16px;
+		}
+
+		.btn-incorrect {
+			background-color: #c04444;
+			/* Green */
+			border: none;
+			color: white;
+			padding: 15px 32px;
+			text-align: center;
+			text-decoration: none;
+			display: inline-block;
+			font-size: 16px;
+		}
+	</style>
+	<script>
+
+
+		var gateway = `ws://192.168.0.106/ws`;
+		var websocket;
+		var scores;
+		var currentPlayer;
+		function initWebSocket() {
+			console.log('Trying to open a WebSocket connection...');
+			websocket = new WebSocket(gateway);
+			websocket.onopen = onOpen;
+			websocket.onclose = onClose;
+			websocket.onmessage = onMessage; // <-- add this line
+		}
+		function onOpen(event) {
+			console.log('Connection opened');
+		}
+
+		function onClose(event) {
+			console.log('Connection closed');
+			setTimeout(initWebSocket, 2000);
+		}
+
+		window.addEventListener('load', onLoad);
+
+		function onLoad(event) {
+			initWebSocket();
+		}
+
+
+
+		var scorePlusVal = 0;
+		var scoreNegativeVal = false;
+		var scoreMinusVal = 0;
+
+		document.addEventListener("DOMContentLoaded", () => {
+			var scorePlus = document.getElementById('scorePlus');
+			var scoreNegative = document.getElementById('scoreNegative');
+			var scoreMinus = document.getElementById('scoreMinus');
+
+			scorePlus.addEventListener('input', (e) => {
+				scorePlusVal = e.target.value;
+				console.log(scorePlusVal);
+			});
+			scoreNegative.addEventListener('change', (e) => {
+				scoreNegativeVal = e.target.checked;
+				console.log(scoreNegativeVal);
+			});
+
+			scoreMinus.addEventListener('input', (e) => {
+				scoreMinusVal = e.target.value;
+				console.log(scoreMinusVal);
+			});
+		});
+
+
+
+		function answer(type) {
+			// websocket.send(`${type},${scorePlusVal},${scoreNegativeVal},${scoreMinusVal}`);
+			websocket.send(type)
+		}
+
+		function onMessage(event) {
+			scores = JSON.parse(event.data);
+			currentPlayer = scores.currentPlayer;
+			console.log(scores);
+
+			if (currentPlayer == 0) {
+				var elScores = document.getElementsByClassName('score');
+				for (let item of elScores) {
+					item.classList.remove('current-player');
+				}
+			} else {
+				document.getElementById(`p${currentPlayer}-score`).classList.add('current-player');
 			}
 
-			.flex-container {
-				display: flex;
-			}
-		</style>
-		<script defer>
+			document.getElementById('p1-score').innerHTML = scores.scores[0];
+			document.getElementById('p2-score').innerHTML = scores.scores[1];
+			document.getElementById('p3-score').innerHTML = scores.scores[2];
+			document.getElementById('p4-score').innerHTML = scores.scores[3];
+		}
 
-		</script>
-	</head>
-	<body>
-		<center><h1>Cerdas Cermat</h1></center>
-		<div class="flex-container">
-			<span>Player 1</span>
-		</div>
-	</body>
+	</script>
+</head>
+
+<body>
+	<h1 class="center">Cerdas Cermat</h1>
+	<div class="grid-container">
+		<div class="grid-content group-name">Group 1</div>
+		<div class="grid-content group-name">Group 2</div>
+		<div class="grid-content group-name">Group 3</div>
+		<div class="grid-content group-name">Group 4</div>
+		<div id="p1-score" class="grid-content score">0</div>
+		<div id="p2-score" class="grid-content score">0</div>
+		<div id="p3-score" class="grid-content score">0</div>
+		<div id="p4-score" class="grid-content score">0</div>
+	</div>
+
+	<div class="flex-container fc-space">
+		<button onclick="answer('correct')" class="btn-correct">Correct</button>
+		<button onclick="answer('incorrect')" class="btn-incorrect">Incorrect</button>
+	</div>
+
+	<div class="flex-container fc-space">
+		<label for="scorePlus">Increase score by:</label>
+		<input type="text" id="scorePlus" name="scorePlus" value="10" disabled
+	</div>
+
+	<div class="flex-container fc-space">
+		<input type="checkbox" id="scoreNegative" name="scoreNegative" disabled>
+		<label for="scoreNegative">Decrease score on incorrect</label>
+	</div>
+
+	<div class="flex-container" style="margin-top: 5px;gap: 10px;">
+		<label for="scoreMinus">Decrease score by:</label>
+		<input type="text" id="scoreMinus" name="scoreMinus" value="10" disabled>
+	</div>
+
+</body>
+
 </html>
-	)+";
-
-
+)+";
 
 void notifyClients() {
 	ws.textAll("{\"currentPlayer\":" + String(currentPlayer) + ",\"scores\":[" +
 			   String(p1_score) + ", " + String(p2_score) + ", " +
 			   String(p3_score) + ", " + String(p4_score) + "]}");
+}
+
+void nextRound() {
+	currentPlayer = 0;
+	digitalWrite(p1_lamp, LOW);
+	digitalWrite(p2_lamp, LOW);
+	digitalWrite(p3_lamp, LOW);
+	digitalWrite(p4_lamp, LOW);
+	notifyClients();
 }
 
 void handleWebSocketMessage(void *arg, uint8_t *data, size_t len) {
@@ -85,11 +266,23 @@ void handleWebSocketMessage(void *arg, uint8_t *data, size_t len) {
 		info->opcode == WS_TEXT) {
 		data[len] = 0;
 		if (strcmp((char *)data, "correct") == 0) {
-			notifyClients();
+			if (currentPlayer == 1) p1_score = p1_score + 10;
+			if (currentPlayer == 2) p2_score = p2_score + 10;
+			if (currentPlayer == 3) p3_score = p3_score + 10;
+			if (currentPlayer == 4) p4_score = p4_score + 10;
+			nextRound();
 		}
 		if (strcmp((char *)data, "incorrect") == 0) {
-			notifyClients();
+			nextRound();
 		}
+
+
+		// char *d = strtok((char*)data, ",");
+		// while (d != NULL) {
+		// 	Serial.println(d);
+		// 	d = strtok(NULL, ",");
+		// }
+
 	}
 }
 
@@ -106,6 +299,8 @@ void onEvent(AsyncWebSocket *server, AsyncWebSocketClient *client,
 			break;
 		case WS_EVT_DATA:
 			handleWebSocketMessage(arg, data, len);
+			Serial.printf("WebSocket data from #%u (%u bytes): %s\n",
+						  client->id(), len, (char *)data);
 			break;
 		case WS_EVT_PONG:
 		case WS_EVT_ERROR:
@@ -116,15 +311,6 @@ void onEvent(AsyncWebSocket *server, AsyncWebSocketClient *client,
 void initWebSocket() {
 	ws.onEvent(onEvent);
 	server.addHandler(&ws);
-}
-
-void nextRound() {
-	currentPlayer = 0;
-	digitalWrite(p1_lamp, LOW);
-	digitalWrite(p2_lamp, LOW);
-	digitalWrite(p3_lamp, LOW);
-	digitalWrite(p4_lamp, LOW);
-	notifyClients();
 }
 
 void setup() {
