0584a17522d315ab9acd785feeec4ac52eb9b064
Waoweens <30313089+Waoweens@users.noreply.github.com>
2022-08-11 19:55:21 +0700

    web server edition: part 1

--- src/main.cpp
+++ src/main.cpp
@@ -4,128 +4,201 @@
  * @version 1.0
  * @date 2022-08-06
  * @copyright Copyright (c) 2022 Cendekia Leadership School. MIT License
- * 
+ *
  */
+#include <ESP8266WiFi.h>
+#include <ESPAsyncTCP.h>
+#include <ESPAsyncWebServer.h>
 
 #include "Arduino.h"
+// #include <LiquidCrystal_I2C.h>
+#include <ESPAsyncTCP.h>
 #include <Wire.h>
-#include <LiquidCrystal_I2C.h>
 
-LiquidCrystal_I2C lcd(0x3F, 16, 9);
+// LiquidCrystal_I2C lcd(0x3F, 16, 9);
+AsyncWebServer server(80);
+AsyncWebSocket ws("/ws");
 
-const int p1_btn = 10;
-const int p2_btn = 11;
-const int p3_btn = 12;
-const int p1_lamp = 7;
-const int p2_lamp = 8;
-const int p3_lamp = 9;
-const int bell = 6;
-const int correct = 5;
-const int incorrect = 4;
+const char* ssid = "KooshaWi-Fi1 2.4GHz";
+const char* password = "theawiligar144";
+
+const int p1_btn = 0;
+const int p2_btn = 2;
+const int p3_btn = 14;
+const int p4_btn = 12;
+
+const int p1_lamp = 16;
+const int p2_lamp = 15;
+const int p3_lamp = 5;
+const int p4_lamp = 4;
+
+const int bell = 13;
 
 int currentPlayer = 0;
 int p1_score = 0;
 int p2_score = 0;
 int p3_score = 0;
-int correct_lastState = 0;
-int incorrect_lastState = 0;
+int p4_score = 0;
+
+const char *html_index = R"+(
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<link rel="preconnect" href="https://fonts.googleapis.com">
+		<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+		<link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet">
+		<title>Cerdas Cermat</title>
+
+		<style>
+			* {
+				font-family: 'Roboto', sans-serif;
+			}
+
+			.flex-container {
+				display: flex;
+			}
+		</style>
+		<script defer>
+
+		</script>
+	</head>
+	<body>
+		<center><h1>Cerdas Cermat</h1></center>
+		<div class="flex-container">
+			<span>Player 1</span>
+		</div>
+	</body>
+</html>
+	)+";
+
+
+
+void notifyClients() {
+	ws.textAll("{\"currentPlayer\":" + String(currentPlayer) + ",\"scores\":[" +
+			   String(p1_score) + ", " + String(p2_score) + ", " +
+			   String(p3_score) + ", " + String(p4_score) + "]}");
+}
+
+void handleWebSocketMessage(void *arg, uint8_t *data, size_t len) {
+	AwsFrameInfo *info = (AwsFrameInfo *)arg;
+	if (info->final && info->index == 0 && info->len == len &&
+		info->opcode == WS_TEXT) {
+		data[len] = 0;
+		if (strcmp((char *)data, "correct") == 0) {
+			notifyClients();
+		}
+		if (strcmp((char *)data, "incorrect") == 0) {
+			notifyClients();
+		}
+	}
+}
+
+void onEvent(AsyncWebSocket *server, AsyncWebSocketClient *client,
+			 AwsEventType type, void *arg, uint8_t *data, size_t len) {
+	switch (type) {
+		case WS_EVT_CONNECT:
+			Serial.printf("WebSocket client #%u connected from %s\n",
+						  client->id(), client->remoteIP().toString().c_str());
+			notifyClients();
+			break;
+		case WS_EVT_DISCONNECT:
+			Serial.printf("WebSocket client #%u disconnected\n", client->id());
+			break;
+		case WS_EVT_DATA:
+			handleWebSocketMessage(arg, data, len);
+			break;
+		case WS_EVT_PONG:
+		case WS_EVT_ERROR:
+			break;
+	}
+}
+
+void initWebSocket() {
+	ws.onEvent(onEvent);
+	server.addHandler(&ws);
+}
+
+void nextRound() {
+	currentPlayer = 0;
+	digitalWrite(p1_lamp, LOW);
+	digitalWrite(p2_lamp, LOW);
+	digitalWrite(p3_lamp, LOW);
+	digitalWrite(p4_lamp, LOW);
+	notifyClients();
+}
 
 void setup() {
 	pinMode(p1_btn, INPUT_PULLUP);
 	pinMode(p2_btn, INPUT_PULLUP);
 	pinMode(p3_btn, INPUT_PULLUP);
+	pinMode(p4_btn, INPUT_PULLUP);
 	pinMode(p1_lamp, OUTPUT);
 	pinMode(p2_lamp, OUTPUT);
 	pinMode(p3_lamp, OUTPUT);
+	pinMode(p4_lamp, OUTPUT);
 	pinMode(bell, OUTPUT);
-	pinMode(correct, INPUT_PULLUP);
-	pinMode(incorrect, INPUT_PULLUP);
 
-	lcd.init();
-	lcd.backlight();
-	lcd.setCursor(0, 0);
-	lcd.print("  P1   P2   P3  ");
-	lcd.setCursor(0, 1);
-	lcd.print("  00   00   00  ");
-}
+	Serial.begin(115200);
 
-void nextRound() {
-	// reset current player indicator
-	currentPlayer = 0;
+	WiFi.mode(WIFI_STA);
+	WiFi.begin(ssid, password);
 
-	// turn off all lamps
-	digitalWrite(p1_lamp, LOW);
-	digitalWrite(p2_lamp, LOW);
-	digitalWrite(p3_lamp, LOW);
+	while (WiFi.status() != WL_CONNECTED) {
+		Serial.print(".");
+		delay(500);
+	}
+	Serial.print(WiFi.localIP());
 
-	// update score count
-	lcd.setCursor(0, 1);
-	lcd.print("                ");
-	lcd.setCursor(2, 1);
-	lcd.print((p1_score < 10 && p1_score >= 0) ? "0" + String(p1_score)
-											  : p1_score);
-	lcd.setCursor(7, 1);
-	lcd.print((p2_score < 10 && p2_score >= 0) ? "0" + String(p2_score)
-											  : p2_score);
-	lcd.setCursor(12, 1);
-	lcd.print((p3_score < 10 && p3_score >= 0) ? "0" + String(p3_score)
-											  : p3_score);
+	// lcd.init();
+	// lcd.backlight();
+	// lcd.setCursor(0, 0);
+	// lcd.print(WiFi.localIP());
+
+	initWebSocket();
+	server.on("/", HTTP_GET, [](AsyncWebServerRequest *request) {
+		request->send_P(200, "text/html", html_index);
+	});
+
+	server.onNotFound([](AsyncWebServerRequest *request) {
+		request->send_P(404, "text/plain", "Not found");
+	});
+
+	server.begin();
+	nextRound();
 }
 
 void loop() {
+	ws.cleanupClients();
+
 	if (digitalRead(p1_btn) == LOW) {
-		if (currentPlayer != 0 && currentPlayer != 1) {
-			return;
-		}
+		if (currentPlayer != 0 && currentPlayer != 1) return;
+
 		currentPlayer = 1;
 		digitalWrite(p1_lamp, HIGH);
 		tone(bell, 1000, 500);
+		notifyClients();
 	}
 
 	if (digitalRead(p2_btn) == LOW) {
-		if (currentPlayer != 0 && currentPlayer != 2) {
-			return;
-		}
+		if (currentPlayer != 0 && currentPlayer != 2) return;
 		currentPlayer = 2;
 		digitalWrite(p2_lamp, HIGH);
 		tone(bell, 2000, 500);
+		notifyClients();
 	}
 
 	if (digitalRead(p3_btn) == LOW) {
-		if (currentPlayer != 0 && currentPlayer != 3) {
-			return;
-		}
+		if (currentPlayer != 0 && currentPlayer != 3) return;
 		currentPlayer = 3;
 		digitalWrite(p3_lamp, HIGH);
 		tone(bell, 3000, 500);
+		notifyClients();
 	}
-
-	if (digitalRead(correct) != correct_lastState) {
-		if (digitalRead(correct) == LOW) {
-			if (currentPlayer == 1) {
-				p1_score++;
-			} else if (currentPlayer == 2) {
-				p2_score++;
-			} else if (currentPlayer == 3) {
-				p3_score++;
-			}
-			nextRound();
-		}
-
+	if (digitalRead(p4_btn) == LOW) {
+		if (currentPlayer != 0 && currentPlayer != 4) return;
+		currentPlayer = 4;
+		digitalWrite(p4_lamp, HIGH);
+		tone(bell, 4000, 500);
+		notifyClients();
 	}
-	correct_lastState = digitalRead(correct);
-
-	if (digitalRead(incorrect) != incorrect_lastState) {
-		if (digitalRead(incorrect) == LOW) {
-			// if (currentPlayer == 1) {
-			// 	p1_score--;
-			// } else if (currentPlayer == 2) {
-			// 	p2_score--;
-			// } else if (currentPlayer == 3) {
-			// 	p3_score--;
-			// }
-			nextRound();
-		}
-	}
-	incorrect_lastState = digitalRead(incorrect);
 }
